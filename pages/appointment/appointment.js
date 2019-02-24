49ad92324ff379d9e7a6abb9b1fe8eeb52495a3a
Una_Bella <369152344@qq.com>
2019-02-24 20:13:53 +0800

    增加店铺预约
    增加店铺预约

---
+++ pages/appointment/appointment.js
@@ -0,0 +1,77 @@
+// pages/appointment/appointment.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    address:'中山路129-3号柏威年购物广场3层东区L3060层（东区L3060）',
+    phone:'400-000-000',
+    list:[
+      {
+        img:'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/A-test/photo.png',
+        teacher:'MMM老师',
+        title:'艺术总监'
+      }, {
+        img: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/A-test/photo.png',
+        teacher: 'MMM老师',
+        title: '艺术总监'
+      }, {
+        img: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/A-test/photo.png',
+        teacher: 'MMM老师',
+        title: '艺术总监'
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+ 
+})
